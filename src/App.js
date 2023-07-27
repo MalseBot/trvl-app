3cf1214228a74b88c63e13ca8f8d7136e138c945
Eslam Hassanin <110671544+MalseBot@users.noreply.github.com>
2023-07-27 17:05:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Home, NavBar, Footer } from './Components'
+
 function App() {
   return (
-    <>
-      <Router>
+    <Router>
         <NavBar />
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
         <Footer />
       </Router>
-    </>
   )
 }
 
